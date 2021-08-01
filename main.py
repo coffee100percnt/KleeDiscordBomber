5fafb65ccb1bbb40bebf8a0e877bd3970427b2fe
Tsyba Dmytro <antiendershow221@gmail.com>
2021-08-01 22:48:58 +0300

    some fixes and bang_roles command

--- main.py
+++ main.py
@@ -6,12 +6,24 @@ from discord.utils import get
 
 token = 'a token'
 bot = commands.Bot(command_prefix='%')
-client = discord.Client()
+guild = discord.Guild()
 
 @bot.command()
 async def boom(ctx, arg):
-    ctx.message.delete()
+    await ctx.message.delete()
     booms = 0
-    while arg >= int(booms):
-        ctx.send('BOOM BOOM BAKUDAN! ||@everyone||')
+    while booms <= int(arg):
+        await ctx.send('BOOM BOOM BAKUDAN! ||@everyone||')
         booms += 1
+    booms = 0
+
+@bot.command()
+async def bang_roles(ctx, arg):
+    await ctx.message.delete()
+    bangs = 0
+    while bangs <= int(arg):
+        await guild.add_role(name='Boing, boing, boom!')
+        bangs += 1
+    bangs = 0
+
+bot.run(token)
