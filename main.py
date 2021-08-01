344d893aadc377b96c0ab38c50292f7c9f4cc962
Tsyba Dmytro <antiendershow221@gmail.com>
2021-08-01 21:49:56 +0300

    add boom command

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import discord
+import discord
 from discord.ext import commands
 from asyncio import sleep
 from discord.utils import get
 
 token = 'a token'
 bot = commands.Bot(command_prefix='%')
+client = discord.Client()
 
+@bot.command()
+async def boom(ctx, arg):
+    ctx.message.delete()
+    booms = 0
+    while arg >= int(booms):
+        ctx.send('BOOM BOOM BAKUDAN! ||@everyone||')
+        count += 1
